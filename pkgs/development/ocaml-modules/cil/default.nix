0e45a38e7993a298e7a93c1849aaa336a045feb9
Nguyễn Gia Phong <cnx@loang.net>
2024-04-25 17:56:00 +0900

    ocamlPackages.cil: fix build for OCaml >= 4.06
    References: https://github.com/samee/obliv-c/issues/76#issuecomment-438958209

--- pkgs/development/ocaml-modules/cil/default.nix
+++ pkgs/development/ocaml-modules/cil/default.nix
@@ -1,8 +1,4 @@
-{ lib, stdenv, fetchurl, perl, ocaml, findlib, ocamlbuild }:
-
-if lib.versionAtLeast ocaml.version "4.06"
-then throw "cil is not available for OCaml ${ocaml.version}"
-else
+{ lib, stdenv, fetchurl, perl, ocaml, findlib, ocamlbuild, num }:
 
 stdenv.mkDerivation rec {
   pname = "ocaml-cil";
@@ -12,8 +8,10 @@ stdenv.mkDerivation rec {
     url = "mirror://sourceforge/cil/cil-${version}.tar.gz";
     sha256 = "05739da0b0msx6kmdavr3y2bwi92jbh3szc35d7d8pdisa8g5dv9";
   };
+  patches = [ ./link-num.patch ];
 
   nativeBuildInputs = [ perl ocaml findlib ocamlbuild ];
+  propagatedBuildInputs = [ num ];
 
   strictDeps = true;
 
